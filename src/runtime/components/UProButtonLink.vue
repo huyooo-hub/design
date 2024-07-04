abc4a74f9ba06969c2c2a80fcb9df9aba4670b31
付国强 <434543162@qq.com>
2024-07-04 16:50:50 +0800

    init

--- src/runtime/components/UProButtonLink.vue
+++ src/runtime/components/UProButtonLink.vue
@@ -29,7 +29,7 @@ defineProps({
     :target="blank ? '_blank' : undefined"
   >
     <button
-      class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center"
+      class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-3 text-center"
     >
       <Icon v-if="icon" :name="icon" />
       <ContentSlot :use="$slots.default" unwrap="p ul li" />
