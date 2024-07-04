80715fdc80c1d0ff7bb83ed60249b88fe9133c64
付国强 <434543162@qq.com>
2024-07-04 16:47:33 +0800

    init

--- src/runtime/components/UProButtonLink.vue
+++ src/runtime/components/UProButtonLink.vue
@@ -8,16 +8,16 @@ defineProps({
   color: {
     type: String,
     required: false,
-    default: 'primary',
+    default: "primary",
   },
   href: {
     type: String,
     required: true,
-    default: '',
+    default: "",
   },
   icon: {
     type: String,
-    default: '',
+    default: "",
   },
 });
 </script>
@@ -28,8 +28,12 @@ defineProps({
     :to="href"
     :target="blank ? '_blank' : undefined"
   >
-    <Icon v-if="icon" :name="icon" />
-    <ContentSlot :use="$slots.default" unwrap="p ul li" />
+    <button
+      class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center"
+    >
+      <Icon v-if="icon" :name="icon" />
+      <ContentSlot :use="$slots.default" unwrap="p ul li" />
+    </button>
   </NuxtLink>
 </template>
 
