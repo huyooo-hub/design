58d240fe974dcbcff8b68f1d30f3caa6489fb7fa
付国强 <434543162@qq.com>
2024-07-04 16:49:53 +0800

    init

--- src/runtime/components/UProButtonLink.vue
+++ src/runtime/components/UProButtonLink.vue
@@ -38,57 +38,57 @@ defineProps({
 </template>
 
 <style scoped lang="less">
-.button-link {
-  display: inline-flex;
-  flex: none;
-  align-items: center;
-  border-radius: 12px;
-  transition: color 100ms ease-in-out;
-  font-weight: 700;
-  color: #ffffff;
-  background-color: #fbfbfb;
-  border: 1px solid transparent;
+// .button-link {
+//   display: inline-flex;
+//   flex: none;
+//   align-items: center;
+//   border-radius: 12px;
+//   transition: color 100ms ease-in-out;
+//   font-weight: 700;
+//   color: #ffffff;
+//   background-color: #fbfbfb;
+//   border: 1px solid transparent;
 
-  &:hover {
-    background-color: #ada9a4;
-  }
+//   &:hover {
+//     background-color: #ada9a4;
+//   }
 
-  &:focus {
-    border: 1px solid #fbfbfb;
-  }
+//   &:focus {
+//     border: 1px solid #fbfbfb;
+//   }
 
-  .icon {
-    margin-inline-end: 0.25rem;
-  }
+//   .icon {
+//     margin-inline-end: 0.25rem;
+//   }
 
-  // 尺寸变体
-  &.small {
-    padding: 0.5rem 1rem;
-    font-size: 0.875rem;
-    line-height: 1.25rem;
-  }
+//   // 尺寸变体
+//   &.small {
+//     padding: 0.5rem 1rem;
+//     font-size: 0.875rem;
+//     line-height: 1.25rem;
+//   }
 
-  &.medium {
-    padding: 0.625rem 1.25rem;
-    font-size: 0.875rem;
-    line-height: 1.5rem;
-  }
+//   &.medium {
+//     padding: 0.625rem 1.25rem;
+//     font-size: 0.875rem;
+//     line-height: 1.5rem;
+//   }
 
-  &.large {
-    padding: 0.75rem 1.5rem;
-    font-size: 1.125rem;
-    line-height: 1.75rem;
-  }
+//   &.large {
+//     padding: 0.75rem 1.5rem;
+//     font-size: 1.125rem;
+//     line-height: 1.75rem;
+//   }
 
-  &.giant {
-    padding: 1rem 2rem;
-    font-size: 1.125rem;
-    line-height: 1.75rem;
-  }
+//   &.giant {
+//     padding: 1rem 2rem;
+//     font-size: 1.125rem;
+//     line-height: 1.75rem;
+//   }
 
-  // 透明变体
-  &.transparent {
-    background-color: transparent;
-  }
-}
+//   // 透明变体
+//   &.transparent {
+//     background-color: transparent;
+//   }
+// }
 </style>
